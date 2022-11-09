ee272ae5519c42da7e6cb8bf701ad2c0f38c8062
Kauserul <kauserulislam0055@gmail.com>
2022-11-09 21:36:20 +0600

    add food done

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -6,6 +6,40 @@ import Row from 'react-bootstrap/Row';
 
 
 const AddService = () => {
+    const handleAddFood = e =>{
+        e.preventDefault()
+
+        const form = e.target;
+        const name = form.name.value;
+        const rating = form.rating.value;
+        const img = form.img.value;
+        const des = form.des.value;
+        const price = form.price.value;
+
+        const food = {
+            name, 
+            rating,
+            img,
+            des,
+            price
+        }
+
+        fetch('http://localhost:5000/services', {
+            method : "POST",
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(food)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            if(data.acknowledged){
+                alert('food added')
+            }
+        })
+        
+    }
     return (
         <Form onSubmit={handleAddFood} className='container mt-5 w-50 border p-4 rounded'>
             <Row className="mb-3">
